0dd566fe3c651610430dd56616ea2ba0f00c014a
AlexVillegasC <alexvillegascarranza@gmail.com>
2026-05-07 17:23:24 -0600

    Borrar Controller Action

--- Progra4-dotnet-test/Controllers/WeatherForecastController.cs
+++ Progra4-dotnet-test/Controllers/WeatherForecastController.cs
@@ -29,17 +29,6 @@ namespace Progra4_dotnet_test.Controllers
             })
             .ToArray();
         }
-
-        [HttpGet("v2",Name = "GetWeatherForecastV2")]
-        public IEnumerable<WeatherForecast> GetV2()
-        {   
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+      
     }
 }
